55b22b34b30def903d9daec9687f7d3852ad846d
bitbakedev <coinbakenow@gmail.com>
2025-08-27 20:17:13 +0700

    광고 크기 줄이고 게임 컨테이너 화면 맞춤

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,11 +1,11 @@
 export const getRenderHeight = () => {
-    const maxHeight = Math.min(window.innerHeight - 80, 700);
+    const maxHeight = Math.min(window.innerHeight - 120, 650);
     const screenHeight = window.innerHeight;
     const screenWidth = window.innerWidth - 8;
     const maxWidth = screenHeight * 4 / 7 - 8;
   
     if (maxWidth > screenWidth) {
-      return Math.min(maxHeight, (screenWidth) * 7 / 4);
+      return Math.min(maxHeight, (screenWidth - 16) * 7 / 4);
     }
   
     return Math.min(maxHeight, screenHeight);
