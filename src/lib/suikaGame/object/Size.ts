fdd3acd0a72782a3db1698a63e90e579f7a2c97f
bitbakedev <coinbakenow@gmail.com>
2025-08-30 13:52:00 +0700

    캔버스를 화면 좌우 꽉 차게 확장

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,7 +1,7 @@
 export const getRenderHeight = () => {
     const maxHeight = window.innerHeight - 220; // 상단 120px + 하단 100px
     const screenHeight = window.innerHeight;
-    const screenWidth = window.innerWidth - 20;
+    const screenWidth = window.innerWidth - 40; // 좌우 여백 20px씩
     const maxWidth = screenHeight * 4 / 7 - 8;
   
     if (maxWidth > screenWidth) {
@@ -12,5 +12,5 @@ export const getRenderHeight = () => {
   };
   
   export const getRenderWidth = () => {
-    return Math.min(window.innerWidth - 20, getRenderHeight() * 4 / 7);
+    return Math.min(window.innerWidth - 40, getRenderHeight() * 4 / 7); // 좌우 여백 20px씩
   };
