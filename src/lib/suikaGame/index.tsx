64ef6dd8edcda03ede13200384748ae9bcbeae7b
bitbakedev <coinbakenow@gmail.com>
2025-09-04 11:57:41 +0700

    흔들기 아이콘 경로 수정

--- src/lib/suikaGame/index.tsx
+++ src/lib/suikaGame/index.tsx
@@ -110,7 +110,7 @@ const SuikaGame = () => {
             title={`흔들기 아이템 (${shakeItemCount}개 남음)`}
           >
             <div className={cx('shakeIcon')}>
-              <img src={require('../../resource/shake_shaker_icon_embedded.svg')} alt="흔들기" width="20" height="20" />
+              <img src={require('../../resource/shake_ icon.png')} alt="흔들기" width="20" height="20" />
             </div>
           </div>
         </div>
