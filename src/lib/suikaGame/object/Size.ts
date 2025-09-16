88bfa2827aa8ca07d70992191c73842627b498ee
bitbakedev <coinbakenow@gmail.com>
2025-09-16 19:35:40 +0700

    캔버스 크기 확인 및 조정

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,5 +1,5 @@
 export const getRenderHeight = () => {
-    const maxHeight = window.innerHeight - 220; // 상단 120px + 하단 100px
+    const maxHeight = window.innerHeight - 160; // 상단 80px + 하단 80px
     const screenHeight = window.innerHeight;
     const screenWidth = window.innerWidth - 40; // 좌우 여백 20px씩
     const maxWidth = screenHeight * 4 / 7 - 8;
@@ -12,5 +12,5 @@ export const getRenderHeight = () => {
   };
   
   export const getRenderWidth = () => {
-    return Math.min(window.innerWidth - 40, getRenderHeight() * 4 / 7); // 좌우 여백 20px씩
+    return Math.min(window.innerWidth - 40, getRenderHeight() * 0.57); // 비율 조정
   };
