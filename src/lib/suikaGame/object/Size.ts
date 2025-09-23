9138340bcd0fbbd5ed4011dba1bf66f06a6068e1
bitbakedev <coinbakenow@gmail.com>
2025-09-23 12:49:44 +0900

    게임창 크기 351x672로 고정

--- src/lib/suikaGame/object/Size.ts
+++ src/lib/suikaGame/object/Size.ts
@@ -1,16 +1,7 @@
 export const getRenderHeight = () => {
-    const maxHeight = window.innerHeight - 180; // 상단 80px + 하단 100px
-    const screenHeight = window.innerHeight;
-    const screenWidth = window.innerWidth - 40; // 좌우 여백 20px씩
-    const maxWidth = screenHeight * 4 / 7 - 8;
+    return 672;
+};
   
-    if (maxWidth > screenWidth) {
-      return maxHeight;
-    }
-  
-    return maxHeight;
-  };
-  
-  export const getRenderWidth = () => {
-    return Math.min(window.innerWidth - 40, 500); // 최대 500px, 최소 화면 너비 - 40px
-  };
+export const getRenderWidth = () => {
+    return 351;
+};
